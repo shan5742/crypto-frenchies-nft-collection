0ed8bb3ae44794a3f1ab11e46a800a6b0566145b
shan5742 <asamshan456@gmail.com>
2022-04-05 09:11:33 +0100

    adding opensea metadata and upadting contract address

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x46A98aBA9f850879063a71A3922E562dc13D69Bf";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://crypto-frenchies-nft-collection.vercel.app//api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
